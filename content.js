fe1c15896e90c3052bf8ce3cc78b5c1d5d9d7aa9
brb <brb@osiris.(none)>
2012-06-30 15:42:16 -0700

    Add gitignore, add playPause functionality

--- content.js
+++ content.js
@@ -1,5 +1,10 @@
 function playPause() {
-	console.log("playPause()");
+  console.log("playPause()");
+  if ($('.playButton').is(':visible')) {
+    $('.playButton').click();
+  } else {
+    $('.pauseButton').click();
+  }
 }
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
