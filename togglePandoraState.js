d82e68c9ba4854b3dbd700ba89e8c5e1b4059e58
Alex Chow <alexchow@alexchow-macbookpro.local>
2012-06-30 19:31:55 -0700

    Finished clientResponder for playing

--- togglePandoraState.js
+++ togglePandoraState.js
@@ -1,3 +1,50 @@
+function processResponses(responseMap) {
+  console.log("processing responses");
+  var isPlaying = false;
+  var curTabPlaying = null;   // the tab id of the tab currently playing
+  // calculate if any tab is playing
+  $.each(responseMap, function(indx, tabResponse) {
+    if(tabResponse.isPlaying) {
+      isPlaying = true;
+      curTabPlaying = tabResponse.tabId;
+    }
+  });
+  if (isPlaying) {
+    chrome.tabs.executeScript(curTabPlaying, {
+      file: "content.js"
+    });
+  } else {
+    // nothing playing, Play the most recently viewed Pandora tab
+  }
+}
+
+// Each tab will send its response to an extensionResponder object
+// So we can synchronize the responses
+var extensionResponder = function() {
+  var classDef = {};
+
+  var impl = function(numTabs) {
+    this.numTabsWaitingFor = numTabs;
+    this.tabResponses = {};
+  };
+  impl.prototype.checkInTabState = function(args) {
+    var thisResponder = this;   // allows access to 'this' inside closure
+    // args:
+    //  tabId : integer
+    //  isPlaying: boolean
+    this.tabResponses[args.tabId] = args;
+    this.numTabsWaitingFor -= 1;
+    if (this.numTabsWaitingFor == 0) {
+      // All tabs have checked in. keep going
+      processResponses(this.tabResponses);
+    }
+  };
+  classDef.createNew = function(numTabs) {
+    return new impl(numTabs);
+  };
+  return classDef;
+}();
+
 function togglePandoraState() {
   chrome.tabs.query({"url":"http://www.pandora.com/"}, function(tabs) {
     if (!tabs.length) {
@@ -5,12 +52,24 @@ function togglePandoraState() {
       return;
     }
     console.log("found "+tabs.length+" pandora tabs");
-    var curPlaying = false;
-    var activeTab = null;
+    var responder = extensionResponder.createNew(tabs.length);
     // find if any tab is currently playing
     $.each(tabs, function(indx, tab) {
+      var curTabPlaying = false;
       // install the chrandora message listener on each tab
+      console.log("install chrandora client Responder");
       chrome.tabs.executeScript(tab.id, {"file":"clientResponder.js"});
+      chrome.tabs.sendMessage(
+        tab.id,
+        {"requestType":"isPlaying"},
+        function(response) {
+          console.log("got response from tab");
+          responder.checkInTabState({
+            "tabId": tab.id,
+            "isPlaying": response.isPlaying
+          });
+        }
+      );
     });
   });
 }
